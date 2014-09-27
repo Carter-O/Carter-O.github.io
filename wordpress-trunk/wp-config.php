f822880b76e1e9fc76f26cd1289748cfdcc16a67
Carter-O <xxaxisgamingxx@gmail.com>
2014-09-26 22:42:46 -0400

    editing

--- wordpress-trunk/wp-config.php
+++ wordpress-trunk/wp-config.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+define('DB_NAME', 'Carter-O.github.io');
 
 /** MySQL database username */
-define('DB_USER', 'username_here');
+define('DB_USER', 'Carter-O');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', 'DEstroy21');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'scrubing.tv/wordpress-trunk/');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
